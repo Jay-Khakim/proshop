b2c5c9d14c57c8e7a03a11408cf181569eb18665
Jay Khakim <mgmediajay@gmail.com>
2023-08-25 08:23:26 +0900

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -10,8 +11,8 @@ const App = () => {
         <Container>
           <h1>Welcome to proshop</h1>
         </Container>
-        
       </main>
+      <Footer/>
     
     </>
   )
