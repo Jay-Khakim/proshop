e54524f6fa0bcab97b4f68bba51b4409aebd1ee7
Jay Khakim <mgmediajay@gmail.com>
2023-10-28 18:18:36 +0900

    Auth & user API Slice

---
+++ frontend/src/slices/userApiSlice.js
@@ -0,0 +1,18 @@
+import { USERS_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const userssApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) =>({
+        login: builder.mutation({
+            query: (data) => ({
+                url: USERS_URL/auth,
+                method: 'POST',
+                body: data
+            }),
+        }),
+        
+    })
+})
+
+
+export const {useLoginMutationQuery} = usersApiSlice;
