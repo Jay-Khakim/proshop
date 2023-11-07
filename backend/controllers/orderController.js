119a6a4a9a45b37a8cc54cb828f18b75c44ebd04
Jay Khakim <mgmediajay@gmail.com>
2023-11-07 16:41:06 +0900

    PayPal Setup & Order Paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -77,15 +77,33 @@ const getOrderById = asyncHandler(async(req, res)=>{
 
 
 // @desc    Update order to Paid
-// @router  GET /api/orders/:id/pay
+// @router  PUT /api/orders/:id/pay
 // @access  Private
 
 const updateOrderToPaid = asyncHandler(async(req, res)=>{
-    res.send('update order to Paid');
+    const order = await Order.findById(req.params.id);
+
+    if(order){
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.email_address,
+        }
+
+        const updateOrder = await order.save();
+
+        res.status(200).json(updateOrder)
+    } else{
+        res.status(404)
+        throw new Error('Order not found')
+    }
 })
 
 // @desc    Update order to delivered
-// @router  GET /api/orders/:id/deliver
+// @router  PUT /api/orders/:id/deliver
 // @access  Private
 
 const updateOrderToDelivered = asyncHandler(async(req, res)=>{
