74b22eb878d6c0c0e71e385d5c5965d451875357
Jay Khakim <mgmediajay@gmail.com>
2023-11-17 20:47:47 +0900

    ..

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,7 +8,5 @@ router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 router.route('/:id/reviews').post(protect, createProductReview);
 
-export default router;
-
-
+export default router
 
