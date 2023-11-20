9f3baad65903265cf51f8138b43b84db0f385300
Jay Khakim <mgmediajay@gmail.com>
2023-11-20 09:20:58 +0900

    preparing for production

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,7 @@ import Product from '../models/productModel.js';
 //@route    GET / api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) =>{
-    const pageSize = 8;
+    const pageSize = processs.env.PAGIANTION_LIMIT;
     const page = Number(req.query.pageNumber) || 1;
 
     const keyword = req.query.keyword ? {name: {$regex: req.query.keyword, $options: 'i'}} : {}
